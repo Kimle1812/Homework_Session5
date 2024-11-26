bb58edb23fbd81dca117d383983999a6df446408
Kimle1812 <nguyenthikimle18122003@gmail.com>
2024-11-26 19:51:54 +0700

    Update Homework5.6.cpp

--- Homework5.6.cpp
+++ Homework5.6.cpp
@@ -1,12 +1,8 @@
 #include<stdio.h>
 int main(){
-	int a, b, i;
-	float n;
+	float a, b, c;
+	int i; 
 	do {
-		printf("Moi ban nhap so thu nhat: ");
-		scanf("%d", &a);
-		printf("Moi ban nhap so thu hai: ");
-		scanf("%d", &b);
 		printf("CALCULATOR\n");
 		printf("1.Tong hai so\n");
 		printf("2.Hieu hai so\n");
@@ -15,32 +11,49 @@ int main(){
 		printf("5.Thoat\n");
 		printf("Lua chon cua ban la: ");
 		scanf("%d", &i);
-		switch(i){
-		case 1:
-			float n = a + b;
-			printf("Ket qua cua phep tinh l‡ : %.2f\n", n);
-			break;
-		case 2:
-			float n = a - b;
-			printf("Ket qua cua phep tinh l‡ : %.2f\n", n);
-			break;
-		case 3:
-			float n = a * b;
-			printf("Ket qua cua phep tinh l‡ : %.2f\n", n);
-			break;
-		case 4:
-			float n = a / b;
-			printf("Ket qua cua phep tinh l‡ : %.2f\n", n);
-			break;
-		case 5:
-			printf("Thoat chuong trinh\n");
-			break;
-		default:
-			printf("So nhap khong hop le");
-		
-	} while ( i != 5);
-	
-	return 0; 
-	
-} 
-
+		switch(i) {
+			case 1:
+				printf("Moi ban nhap so thu nhat: ");
+		        scanf("%f", &a);
+		        printf("Moi ban nhap so thu hai: ");
+		        scanf("%f", &b);
+				c = a + b;
+				printf("Ket qua cua phep tinh l√† : %.2f\n", c);
+				break;
+			case 2:
+				printf("Moi ban nhap so thu nhat: ");
+		        scanf("%f", &a);
+		        printf("Moi ban nhap so thu hai: ");
+		        scanf("%f", &b);
+				c = a - b;
+				printf("Ket qua cua phep tinh l√† : %.2f\n", c);
+				break;
+			case 3:
+				printf("Moi ban nhap so thu nhat: ");
+		        scanf("%f", &a);
+		        printf("Moi ban nhap so thu hai: ");
+		        scanf("%f", &b);
+				c = a * b;
+				printf("Ket qua cua phep tinh l√† : %.2f\n", c);
+				break;
+			case 4:
+				printf("Moi ban nhap so thu nhat: ");
+		        scanf("%f", &a);
+		        printf("Moi ban nhap so thu hai: ");
+		        scanf("%f", &b);
+		        if(b == 0 || a == 0){
+		        	printf("Phep chia khong chia duoc cho 0\n");
+				} else {
+					c = a / b;
+				    printf("Ket qua cua phep tinh l√† : %.2f\n", c);
+				}
+				break;
+			case 5:
+				printf("Thoat chuong trinh\n");
+				break;
+			default:
+				printf("So nhap khong hop le\n");
+		}
+	} while ( i != 5 );
+	return 0; 	
+}
